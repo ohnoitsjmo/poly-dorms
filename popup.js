7b02d6136415189488b98419bd24ed954016e609
Justin Mo <jmo02@calpoly.edu>
2018-08-06 20:57:10 -0700

    copy feedbackg

--- popup.js
+++ popup.js
@@ -46,26 +46,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     copyyak.addEventListener('click', function() {
         copy('yakʔitʸutʸu');
+        copyyak.firstChild.data = "Copied!";
+        setTimeout(function(){ copyyak.firstChild.data = "yakʔitʸutʸu"; }, 1500);
     });
     copyelewexe.addEventListener('click', function() {
         copy('elewexe');
+        copyelewexe.firstChild.data = "Copied!";
+        setTimeout(function(){ copyelewexe.firstChild.data = "elewexe"; }, 1500);
     });
     copynip.addEventListener('click', function() {
         copy('nipumūʔ');
+        copynip.firstChild.data = "Copied!";
+        setTimeout(function(){ copynip.firstChild.data = "nipumūʔ"; }, 1500);
     });
     copyti.addEventListener('click', function() {
         copy('tiłhini');
+        copyti.firstChild.data = "Copied!";
+        setTimeout(function(){ copyti.firstChild.data = "tiłhini"; }, 1500);
     });
     copyts.addEventListener('click', function() {
         copy('tsʰɨtqawɨ');
+        copyts.firstChild.data = "Copied!";
+        setTimeout(function(){ copyts.firstChild.data = "tsʰɨtqawɨ"; }, 1500);
     });
     copyt.addEventListener('click', function() {
         copy('tšɨłkukunɨtš');
+        copyt.firstChild.data = "Copied!";
+        setTimeout(function(){ copyt.firstChild.data = "tšɨłkukunɨtš"; }, 1500);
     });
     copytsp.addEventListener('click', function() {
         copy('tsɨpxatu');
+        copytsp.firstChild.data = "Copied!";
+        setTimeout(function(){ copytsp.firstChild.data = "tsɨpxatu"; }, 1500);
     });
     copytst.addEventListener('click', function() {
         copy('tsɨtkawayu');
+        copytst.firstChild.data = "Copied!";
+        setTimeout(function(){ copytst.firstChild.data = "tsɨtkawayu"; }, 1500);
     });
 });
